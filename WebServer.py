b03e40520aa2d1ea0186b5a19de2d0017182fdc6
Hongwoo Yoon <hongwoo.yoon@ucalgary.ca>
2024-10-09 21:54:00 -0600

    Part 1 done

--- WebServer.py
+++ WebServer.py
@@ -1,24 +1,19 @@
 #import socket module
 from socket import *
+import threading
 
-serverSocket = socket(AF_INET, SOCK_STREAM)
-#Prepare a sever socket
-#Fill in start
-#Fill in end
-while True:
-    #Establish the connection
-    print('Ready to serve...')
-    connectionSocket, addr =  #Fill in start #Fill in end
+def handle_client(connectionSocket, addr):
     try:
-        message =  #Fill in start #Fill in end
+        print(f"Accepted connection from {addr[0]}:{addr[1]}")
+        message =  connectionSocket.recv(1024)
         filename = message.split()[1]
         f = open(filename[1:])
 
-        outputdata = # Fill in start # Fill in end
+        outputdata = f.read()
 
         #Send one HTTP header line into socket
-        #Fill in start
-        #Fill in end
+        connectionSocket.send('HTTP/1.x 200 OK\r\n'.encode())
+        connectionSocket.send('Content-Type: text/html\r\n\r\n'.encode())
         #Send the content of the requested file to the client
         for i in range(0, len(outputdata)):
             connectionSocket.send(outputdata[i].encode())
@@ -26,11 +21,24 @@ while True:
         connectionSocket.close()
     except IOError:
         #Send response message for file not found
-        # Fill in start
-        # Fill in end
+        connectionSocket.send('404 Page Not Found\r\n'.encode())
+        connectionSocket.send('Content-Type: text/html\r\n\r\n'.encode())
+        connectionSocket.send('<html><body><h1>404 Page Not Found</h1></body></html>'.encode())
 
         #Close client socket
-        # Fill in start
-        # Fill in end
-serverSocket.close()
+        connectionSocket.close()
 
+serverSocket = socket(AF_INET, SOCK_STREAM)
+#Prepare a sever socket
+serverSocket.bind(('127.0.0.1', 12345))
+serverSocket.listen(1)
+print("Server is listening...")
+while True:
+    #Establish the connection
+    print('Ready to serve...')
+    connectionSocket, addr =  serverSocket.accept() 
+    
+    thread = threading.Thread(target=handle_client, args=(connectionSocket, addr))
+    thread.start()
+
+serverSocket.close()
